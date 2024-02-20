7d8b32f25c9b933952c9e1b143e52ce70d3833dc
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-02-20 17:58:02 +0100

    Addition of a section at the bottom of the main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,47 @@
-import {Header} from "@/components/layout/Header"
-import {Hero} from "@/components/layout/Hero"
+import { Header } from "@/components/layout/Header";
+import { Hero } from "@/components/layout/Hero";
 import { HomeMenu } from "@/components/layout/HomeMenu";
+import { SectionHeader } from "@/components/layout/SectionHeader";
 
 export default function Home() {
   return (
     <>
-    <Header/>
-    <Hero/>
-    <HomeMenu/>
+      <Header />
+      <Hero />
+      <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeader subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-2xl mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Lorem ipsum dolor sit amet. In autem eaque non internos quos aut
+            quos aliquam ut delectus.Lorem ipsum dolor sit amet. In autem eaque
+            non internos quos aut quos aliquam ut delectus.Lorem ipsum dolor sit
+            amet. In autem eaque non internos quos aut quos
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet. In autem eaque non internos quos aut
+            quos aliquam ut delectus.Lorem ipsum dolor sit amet. In autem eaque
+            non internos quos aut quos t delectus.Lorem ipsum dolor sit amet. In
+            autem eaque non internos quos aut quos
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet. In autem eaque non internos quos aut
+            quos aliquam ut delectus.Lorem ipsum dolor sit amet. In autem eaque
+            non internos quos aut quos
+          </p>
+        </div>
+      </section>
+      <section className="text-center">
+        <SectionHeader subHeader={"Don't hesitate"} mainHeader={"Contact us"} />
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-500" href="tel:+0908457052">
+            +09 08 45 70 52
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2024 All right reserved
+      </footer>
     </>
   );
 }
