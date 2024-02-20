47231b22d1ff36cc6cf77328504afa079943d074
Jimmy Catalano <jimmy.catalanoperso@gmail.com>
2024-02-20 17:56:58 +0100

    Creating a reusable component

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,14 +1,11 @@
-import Image from "next/image";
 import { MenuItem } from "../menu/MenuItem";
+import { SectionHeader } from "./SectionHeader";
 
 export const HomeMenu = () => {
   return (
     <section className="">
       <div className="text-center mb-4">
-        <h3 className="upercase text-gray-600 font-semibold leading-4">
-          Check out
-        </h3>
-        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+        <SectionHeader subHeader={'check out'} mainHeader={'Menu'}/>
       </div>
       <div className="grid grid-cols-3 gap-4">
         <MenuItem/>
